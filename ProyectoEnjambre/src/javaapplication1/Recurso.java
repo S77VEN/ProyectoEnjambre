38a5e0844ea20bb1071ac6ac49355ce75f3bad70
S77VEN31 <stivensg313131@estudiantec.cr>
2022-04-26 11:51:24 -0600

    cambios en recursos
    ok

--- ProyectoEnjambre/src/javaapplication1/Recurso.java
+++ ProyectoEnjambre/src/javaapplication1/Recurso.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package javaapplication1;
+import java.util.Random;
 import javax.swing.JLabel;
 
 
@@ -15,20 +16,29 @@ public class Recurso extends Objeto {
     public Recurso (){
         this.salud = 10;
     }
-
+    public int rangoRandom(int rango){
+        Random aleatorio = new Random();
+        int x = aleatorio.nextInt();
+        return x;
+    }
     @Override
     public void desaparecer() {
+        this.imagen.setIcon(null);
         System.out.println("Desaparece xd");
     }
-
     @Override
     public void disminuirSalud() {
-        
+        if (this.salud > 0){
+            this.salud = this.salud - 1; 
+        }
+        else{
+            desaparecer();
+        }
     }
-
     @Override
     public void recolocar() {
-        
+        this.posX = rangoRandom(25);
+        this.posY = rangoRandom(12);
     }
     
 }
